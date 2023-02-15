a75f2863eb6c5292d2a45c3d36235c017aacb7a3
apple <apple@appurunoMacBook-Pro.local>
2023-02-15 20:31:42 +0900

    cors fix for frontend

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ require('dotenv').config();
 const port = process.env.PORT || 8000
 
 //middleware
-app.use(cors());
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) 
 app.use(express.json());
 
 
